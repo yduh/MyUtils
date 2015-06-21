af1490873a74ddf55a85d06b64aaa315cb4b7faf
lucien1011 <lucien1011@gmail.com>
2015-06-21 10:47:48 +0200

    added template function for dividing histograms

--- makeRatio.C
+++ makeRatio.C
@@ -1,30 +1,12 @@
-#include "TFile.h"
 
-void makeRatioHisto(){
+template <typename T>
+T* makeRatio(const T* num,const T* dem){
+	T* ratio = (T*)num -> Clone();
+	ratio -> Divide(dem);
 
-	TFile * inputFile = new TFile("output/validation.root");
-	TH2D * nom = inputFile->Get("QCD_HT_1000ToInf/h_jet_etaphi_BiasedDPhil0p3");
-	TH2D * dem = inputFile->Get("QCD_HT_1000ToInf/h_jet_etaphi_BiasedDPhig0p3");
+	ratio -> SetStats(0);
 
-	TH2D * ratio = nom->Clone();
-	ratio->Divide(dem);
-
-	ratio->SetStats(0);
-	ratio->SetTitle("Ratio with number of biasedDPhi Jets to the number of all jets");
-	TCanvas * c1 = new TCanvas();
-	ratio->Draw("colz");
+	return ratio;
+}
 
 
-	TH1D * ratio1D = new TH1D("ratio1D","1D Ratio",50,0.,1.);
-	double temp=0;
-	for (unsigned xbin = 0; xbin < ratio->GetNbinsX(); xbin++){
-		for (unsigned ybin = 0; ybin < ratio->GetNbinsY(); ybin++){
-			ratio1D->Fill(ratio->GetBinContent(xbin,ybin));
-		};
-	};
-	TCanvas * c2 = new TCanvas();
-	ratio1D->SetStats(0);
-	ratio1D->Draw();
-
-
-};
