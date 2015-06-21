238634f9d7f8d8ea8947d50d922b81d38526ceb6
lucien1011 <lucien1011@gmail.com>
2015-06-21 10:53:48 +0200

    further tidy up folder

--- Reference/pyroot.py
+++ Reference/pyroot.py
@@ -1,23 +1,6 @@
 import os
 import ROOT as r
 
-class CFG(object):
-    '''Base configuration class. The attributes are used to store parameters of any type'''
-    def __init__(self, **kwargs):
-        '''All keyword arguments are added as attributes.'''
-        self.__dict__.update( **kwargs )
-        
-    def __str__(self):
-        '''A useful printout'''
-        header = '{type}: {name}'.format( type=self.__class__.__name__,
-                                          name=self.name)
-        varlines = ['\t{var:<15}:   {value}'.format(var=var, value=value) \
-                    for var,value in sorted(vars(self).iteritems()) \
-                    if var is not 'name']
-        all = [ header ]
-        all.extend(varlines)
-        return '\n'.join( all )
-
 #Get the names of all files under a directory
 def getfilelist(dir=""):
 	filelist=[]
